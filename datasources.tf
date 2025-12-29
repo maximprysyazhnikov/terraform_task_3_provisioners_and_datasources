fda9e56379928fb7fee59a854624962d18d57bf5
maximprysyazhnikov <maximprysyazhnikov@users.noreply.github.com>
2025-12-29 22:45:15 +0200

    Fix: move all datasources to datasources.tf as required

--- datasources.tf
+++ datasources.tf
@@ -1,3 +1,27 @@
 data "azurerm_resource_group" "rg" {
   name = var.resource_group_name
 }
+
+#####################################
+# Datasources (task requirement)
+#####################################
+data "azurerm_virtual_network" "vnet" {
+  name                = azurerm_virtual_network.vnet.name
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+data "azurerm_subnet" "subnet" {
+  name                 = azurerm_subnet.subnet.name
+  virtual_network_name = data.azurerm_virtual_network.vnet.name
+  resource_group_name  = data.azurerm_resource_group.rg.name
+}
+
+data "azurerm_network_interface" "nic" {
+  name                = azurerm_network_interface.nic.name
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
+
+data "azurerm_virtual_machine" "vm" {
+  name                = azurerm_linux_virtual_machine.vm.name
+  resource_group_name = data.azurerm_resource_group.rg.name
+}
